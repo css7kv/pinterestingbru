474c77b6741e69616c4f088176275d08e33e80d4
css7kv <css7kv@virginia.edu>
2015-06-04 09:18:36 +0200

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150603163355) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
@@ -21,14 +24,14 @@ ActiveRecord::Schema.define(version: 20150603163355) do
     t.integer  "user_id"
   end
 
-  add_index "boards", ["user_id"], name: "index_boards_on_user_id"
+  add_index "boards", ["user_id"], name: "index_boards_on_user_id", using: :btree
 
   create_table "boards_pins", id: false, force: :cascade do |t|
     t.integer "pin_id",   null: false
     t.integer "board_id", null: false
   end
 
-  add_index "boards_pins", ["pin_id", "board_id"], name: "index_boards_pins_on_pin_id_and_board_id"
+  add_index "boards_pins", ["pin_id", "board_id"], name: "index_boards_pins_on_pin_id_and_board_id", using: :btree
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"
@@ -39,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150603163355) do
     t.string   "url"
   end
 
-  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -56,7 +59,9 @@ ActiveRecord::Schema.define(version: 20150603163355) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "boards", "users"
+  add_foreign_key "pins", "users"
 end
